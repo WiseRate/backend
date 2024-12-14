7c6ed7f741de1b59f531eac5ab70739520516324
Himanshu <jain2098@gmail.com>
2024-12-14 18:18:56 -0500

    update

--- src/main/java/com/wiserate/controller/FrontendController.java
+++ src/main/java/com/wiserate/controller/FrontendController.java
@@ -1,5 +1,6 @@
 package com.wiserate.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +10,10 @@ public class FrontendController {
 
     // Path not start with /api
     @GetMapping(value = "/{path:[^\\.]*}")
-    public String forward(@PathVariable String path) {
+    public String forward(@PathVariable String path, HttpServletRequest request) {
+        if (request.getRequestURI().startsWith("/api/h2-console")) {
+            return "forward:/api/h2-console";
+        }
         return "forward:/index.html";
     }
 }
