5823f8539d12c2d8d4c4f6a5340a4fccb3351d3f
Himanshu <jain2098@gmail.com>
2024-12-14 19:38:41 -0500

    update

--- src/main/java/com/wiserate/config/SecurityConfig.java
+++ src/main/java/com/wiserate/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://wiserate-b64eaf61bfea.herokuapp.com", "http://127.0.0.1:3000")
+                        .allowedOrigins("http://localhost:3000", "https://wiserate-b64eaf61bfea.herokuapp.com", "http://127.0.0.1:3000", "https://mortgage.himanshujain.me")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true)
